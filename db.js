d51d0c6723f18a8beebbed6a9abe2513ebe84bb1
Luis Irizarry <lirizarry6178@gmail.com>
2024-12-09 19:22:02 -0500

    start of project

---
+++ db.js
@@ -0,0 +1,29 @@
+"use strict";
+/** Database setup for jobly. */
+const { Client } = require("pg");
+const { getDatabaseUri } = require("./config");
+
+let db;
+
+if (process.env.NODE_ENV === "production") {
+  db = new Client({
+    connectionString: getDatabaseUri(),
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+} else {
+  db = new Client({
+    connectionString: getDatabaseUri()
+  });
+}
+
+db.connect()
+  .then(() => {
+    console.log(`Starting server with DB:${getDatabaseUri()}`)
+  })
+  .catch((err) => {
+    console.log(``)
+  })
+
+module.exports = db;
